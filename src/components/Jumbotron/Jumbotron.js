cd7649a144c714d25f3fd64d33ec49e25dac6bff
mohebifar <netfars@gmail.com>
2016-05-08 16:51:44 +0430

    Update line

--- src/components/Jumbotron/Jumbotron.js
+++ src/components/Jumbotron/Jumbotron.js
@@ -4,8 +4,8 @@ import styles from './Jumbotron.scss';
 export default class Jumbotron extends Component {
   render() {
     return (<header className={styles.wrapper}>
-      <h1>جام فوتبال بین استارتاپ‌ها</h1>
-      <h2>بسه هرچقدر کد زدی و کار کردی، یه ذره هم تفریح و رقابت کن!</h2>
+      <h1>۴۰ دقیقه بدون اینترنت</h1>
+      <h2>جام فوتبال سالنی بین استارتاپ‌ها</h2>
     </header>);
   }
 }
